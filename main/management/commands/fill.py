d03f924b8f6b77c8d1b3edc3141d2070e19c0a1d
evgeniia lemanova <jane87.05@mail.ru>
2023-06-01 21:09:15 +0300

    add README

--- main/management/commands/fill.py
+++ main/management/commands/fill.py
@@ -5,24 +5,22 @@ from main.models import Product
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
         product_list = [
             {'name': 'apple', 'price': 130, 'category': 'fruits'},
             {'name': 'cucumber', 'price': 120, 'category': 'vegetables'},
             {'name': 'chicken', 'price': 100, 'category': 'meat'},
-            {'name': 'fish', 'price': 700, 'category': 'seafood'},
+            {'name': 'mussels', 'price': 700, 'category': 'seafood'},
         ]
-
 
         product_for_create = []
 
-
-
-
-        for product in product_list:
-            if product['name'] not in old_products:
+        if Product.objects.all() is not None:
+            Product.objects.all().delete()
+            for product in product_list:
                 product_for_create.append(
                     Product(**product)
                 )
-            # elif product['name'] in old_products:
-
-        Product.objects.update_or_create(product_for_create)
+            Product.objects.bulk_create(product_for_create)
+        else:
+            Product.objects.bulk_create(product_for_create)
