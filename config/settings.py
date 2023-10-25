14532b2f759b7fb06aedff846f3659662ec3bef6
evgeniia <jane87.05@mail.ru>
2023-10-25 23:24:25 +0300

    start 23.1

--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,10 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379",
     }
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
